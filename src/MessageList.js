7d9d08afd866d44ef832f09ae798e9c65760d4c1
Benjamin Rich <benlr98@gmail.com>
2018-06-12 10:16:03 -0400

    constructed messages as an object to call within MessageList

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const MessageList = () => {
+    const messages = [
+        {id: 1 , username: 'benlr98', body: 'Chatting away!'},
+        {id: 2 , username: 'billy', body: 'hahah'},
+    ]
+
+
     return(
         <div className="MessageList">
-            MessageList
+            {
+            messages.map(message => (
+                <div key={message.id}>
+                    {message.username}: {message.body}
+                </div>
+                ))
+            }
         </div>
     )
 }
