a29d710f54e0e7bda44582837e0ff7038a1c0ade
Benjamin Rich <benlr98@gmail.com>
2018-06-12 10:07:04 -0400

    adds MessageList

---
+++ src/MessageList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MessageList = () => {
+    return(
+        <div className="MessageList">
+            MessageList
+        </div>
+    )
+}
+
+export default MessageList;
